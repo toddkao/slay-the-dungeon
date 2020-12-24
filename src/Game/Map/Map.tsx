121aedc14aab7f589f24262b915502105c38f53a
toddkao <todd.kakao@gmail.com>
2020-12-24 03:54:58 -0500

    WIP randomly generating nodes on a map

--- src/Game/Map/Map.tsx
+++ src/Game/Map/Map.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Row } from '../../Layout';
-import { Modal } from '../Common/Modal';
-import { Player } from '../Entities/Player';
+import React from "react";
+import styled from "styled-components";
+import { Column, Row } from "../../Layout";
+import { Modal } from "../Common/Modal";
+import { Player } from "../Entities/Player";
+import { range } from "lodash";
 
 const Wrapper = styled(Row)`
   width: 100vw;
@@ -11,15 +12,36 @@ const Wrapper = styled(Row)`
 
 const MapNode = styled.div`
   background-color: black;
+  border-radius: 50%;
   height: 40px;
   width: 40px;
+  position: absolute;
 `;
 
 export function Map() {
   const player = new Player();
+  const layersOfNodes = range(0, 16);
+
   return (
     <Modal>
-      <MapNode />
+      <Column style={{ gap: 200, maxHeight: "80vh", overflow: "auto" }}>
+        {layersOfNodes.map((layer) => {
+          const randomNumberOfNodes = Math.floor(Math.random() * 3) + 2;
+          return (
+            <Row style={{ gap: 100, position: "relative", width: "40vw" }}>
+              {range(0, randomNumberOfNodes).map((index) => (
+                <MapNode
+                  key={index}
+                  style={{
+                    left: (Math.floor(Math.random() * 30) + 150) * (index + 1),
+                    top: (Math.floor(Math.random() * 30) + 100),
+                  }}
+                />
+              ))}
+            </Row>
+          );
+        })}
+      </Column>
     </Modal>
   );
 }
