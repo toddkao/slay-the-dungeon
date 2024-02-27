ab16c8a6dc3dfde85a983bab443f6fe21cbdcee3
todd kao <todd.kakao@gmail.com>
2024-02-27 18:39:04 -0500

    Add block icon on health bar to display block

--- src/Game/Common/HealthBar.tsx
+++ src/Game/Common/HealthBar.tsx
@@ -2,18 +2,26 @@ import { observer } from "mobx-react";
 import styled from "styled-components";
 import { horizontalCenterAbsolute, Row } from "../../Layout";
 import { Typography } from "../../Typography";
-import React from 'react';
+
+import block from "../../Images/combat/blockAnimation.png";
 
 export const HealthBar = observer((props: IHealthBarProps) => (
-  <Bar>
+  <Bar hasShield={!!props.block}>
+    {!!props.block && (
+      <BlockIcon>
+        <Row align="center" justify="center" style={{height: '100%', width: '100%'}}>
+          <BlockNumber fontSize={18} outline>
+            {props.block}
+          </BlockNumber>
+        </Row>
+      </BlockIcon>
+    )}
     <HealthNumber fontSize={18} outline>
-      {props.health}
-      {props.block ? `(${props.block})` : ""} / {props.maxHealth}
+      {props.health} / {props.maxHealth}
     </HealthNumber>
     <Health healthPercentage={(props.health / props.maxHealth) * 100} />
   </Bar>
 ));
-
 
 const Health = styled.div<{ healthPercentage: number }>`
   position: absolute;
@@ -34,12 +42,31 @@ const HealthNumber = styled(Typography)`
   padding: 3px;
 `;
 
-const Bar = styled(Row)`
+const BlockIcon = styled.div`
+  background: url(${block});
+  background-size: contain;
+  width: 50px;
+  height: 50px;
+  top: -20px;
+  left: -20px;
+  z-index: 1;
+  position: absolute;
+`;
+
+const BlockNumber = styled(Typography)`
+  color: white;
+  white-space: nowrap;
+  z-index: 1;
+  line-height: 0;
+`;
+
+const Bar = styled(Row)<{hasShield?: boolean}>`
   position: relative;
   width: 100%;
   background-color: black;
   border-radius: 4px;
   height: 5px;
+  ${({ hasShield }) => (hasShield ? `border: 2px solid lightblue` : "")};
 `;
 
 interface IHealthBarProps {
